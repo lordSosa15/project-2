4fdae97c495c8ab8ab463125d273f06bf6c713fb
lordSosa15 <97013669+lordSosa15@users.noreply.github.com>
2022-04-07 21:03:04 -0400

    third commit

---
+++ config/route-guard.config.js
@@ -0,0 +1,28 @@
+
+    
+    //what happens if user is logged
+    //what user can do if they are logged in 
+
+    const isLoggedIn = (req, res, next) => {
+        if(!req.session.currentUser){
+            return res.redirect("/login");   
+        }
+        next()
+    }
+
+    //what user can do if they are logged out
+
+
+    const isLoggedOut = (req, res, next) => {
+        //if is not logged in
+        console.log(req.session.currentUser)
+        if(req.session.currentUser){
+            return res.redirect("/")
+        }
+        next()
+    }
+
+    module.exports = {
+        isLoggedIn,
+        isLoggedOut
+    }
---
+++ config/session.config.js
@@ -0,0 +1,28 @@
+const session = require("express-session");
+
+const MongoStore = require("connect-mongo");
+
+const mongoose = require("mongoose")
+
+module.exports = (app) => { //app is a place holder
+
+    app.set('trust proxy', 1) // important in deployment
+
+    app.use(session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { 
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+            secure: process.env.NODE_ENV === 'production',
+            httpOnly: true,
+            maxAge: 60000 // 60 * 1000 ms === 1 min  BANK is usually 10 mins 
+    },
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/project2'
+ 
+        // ttl => time to live
+        // ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
+    })
+}))
+}
