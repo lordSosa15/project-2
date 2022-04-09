58a622e1eea0ace5a11f6c25ef9101eb3fc419ae
lordSosa15 <97013669+lordSosa15@users.noreply.github.com>
2022-04-09 14:40:31 -0400

    session change

--- config/session.config.js
+++ config/session.config.js
@@ -18,7 +18,7 @@ module.exports = (app) => {
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         secure: process.env.NODE_ENV === "production",
         httpOnly: true,
-        maxAge: 60000, // 60 * 1000 ms === 1 min  BANK is usually 10 mins
+        maxAge: 6000000000000, // 60 * 1000 ms === 1 min  BANK is usually 10 mins
       },
       store: MongoStore.create({
         mongoUrl: process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/project2",
